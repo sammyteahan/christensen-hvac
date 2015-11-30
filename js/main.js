05ce367a74dc12cf3812d3972bd44fc4de1ffc65
sammyteahan <sam.teahan@aggiemail.usu.edu>
2015-11-29 22:21:25 -0700

    Ive already gone way to long without committing

--- js/main.js
+++ js/main.js
@@ -10,6 +10,14 @@
     offset: 0
   });
 
+  var icon = document.querySelector('.menu-icon-wrapper');
+  var menu = document.querySelector('.wrap-mobile-menu');
+
+  var toggleClass = function() {
+    menu.classList.toggle('is-active');
+  }
+
+  icon.addEventListener('click', toggleClass);
+
   console.log('-- js loaded --');
-  
 }());
