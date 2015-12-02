e88085268ff0562ef9139eb04cae1366cf739e8c
sammyteahan <sam.teahan@aggiemail.usu.edu>
2015-12-02 08:06:26 -0700

    service area, contact, and product sections

--- js/main.js
+++ js/main.js
@@ -12,9 +12,16 @@
 
   var icon = document.querySelector('.menu-icon-wrapper');
   var menu = document.querySelector('.wrap-mobile-menu');
+  var menuList = document.querySelector('.wrap-mobile-menu > ul');
 
   var toggleClass = function() {
-    menu.classList.toggle('is-active');
+    if(!menu.clientHeight) {
+      menu.style.height = 166 + 'px';
+      menuList.style.marginTop = 0;
+    } else {
+      menu.style.height = 0;
+    }
+    // menu.classList.toggle('is-active');
   }
 
   icon.addEventListener('click', toggleClass);
